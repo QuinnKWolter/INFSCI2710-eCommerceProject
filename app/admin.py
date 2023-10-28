a90b6a04410eca1bb2bc1c8291fadc1471e24c26
Quinn K Wolter <QuinnKWolter@gmail.com>
2023-10-28 17:39:13 -0400

    Update Models.py

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-#admin.site.register(MODEL_NAME_HERE!)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Salesperson)
+admin.site.register(Store)
+admin.site.register(Region)
+admin.site.register(Customer)
