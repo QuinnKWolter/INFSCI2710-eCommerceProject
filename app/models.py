6a538ae87162f32f9969fd58f440af0724b1c586
Abhigyan Kishor <ak1909552@qu.edu.qa>
2023-10-29 19:08:28 -0400

    added gitignore for csvs. Created scripts for inserting products and categories

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Category Model
 class Category(models.Model):
+    unique_id = models.IntegerField(null = True)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
 
@@ -15,7 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(default=10)
+    # stock = models.IntegerField(default=10)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
---
+++ scripts/insert_product.py
@@ -0,0 +1,15 @@
+import pandas 
+from app.models import Product, Category
+
+electronics = pandas.read_csv('electronics.csv')
+
+for row in electronics.itertuples():
+    prod = Product(
+        name = row.asin,
+        description = row.title,
+        price = row.price,
+        # stock = 10,
+        category = Category.objects.get(unique_id = row.category_id),
+        image = row.imgUrl
+    )
+    prod.save()
