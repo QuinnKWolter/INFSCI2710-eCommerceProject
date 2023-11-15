486cf6e23d3fa017288b4e957e830405ea247afc
Abhigyan Kishor <ak1909552@qu.edu.qa>
2023-11-15 18:51:02 -0500

    merging logan

---
+++ scripts/insert_inventory.py
@@ -0,0 +1,11 @@
+from app.models import Product, Category, Review, Customer, Store, Inventory
+import random
+products = Product.objects.all()
+stores = Store.objects.all()
+for product in products:
+    for store in stores:
+        inven = Inventory(quantity = random.randint(0, 100),
+                          store = store,
+                          product = product
+                          )
+        inven.save()
