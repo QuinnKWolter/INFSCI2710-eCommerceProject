f9ff3ca72252d0710f27ad568ae7547c5ad36291
Quinn K Wolter <QuinnKWolter@gmail.com>
2023-11-28 23:16:30 -0500

    Pre-demo bugfixing spree. Category images repopulated, review stars standardized, prices fixed, descriptions axed.

--- app/models.py
+++ app/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 
 from django.db.models import Avg, Count, Min, Sum
 from django.contrib.auth.models import Permission
+from django.core.validators import RegexValidator
 
 # Category Model
 class Category(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='category_images/', blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -26,7 +27,7 @@ class Category(models.Model):
 # quick note for aggregation 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
@@ -143,7 +144,8 @@ class Region(models.Model):
 # Customer Model
 class Customer(User):
     name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=15, blank = True, null= True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True)
     street_address = models.CharField(max_length=300, blank = True, null= True)
     city = models.CharField(max_length=100, blank = True, null= True)
     state = models.CharField(max_length=100,  blank = True, null= True)
--- scripts/insert_category_images.py
+++ scripts/insert_category_images.py
@@ -5,5 +5,5 @@ categories = list(Category.objects.all())
 categories.reverse()
 
 for i, category in enumerate(categories):
-    category.image = os.path.join('category_images', f'{i+1}.jpeg')
+    category.image = os.path.join(os.sep, 'media/category_images' + os.sep, f'{i+1}.jpeg')
     category.save()
